cbe3e8199934c5ec60fd01d20e9c84e0cc9c4582
melimelo17 <maden.tiercepretet@hotmail.com>
2024-11-01 08:23:26 +0100

    Items

--- src/main/java/net/melimelo17/tutorialmod/TutorialMod.java
+++ src/main/java/net/melimelo17/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.melimelo17.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.melimelo17.tutorialmod.Items.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -32,6 +34,9 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -45,7 +50,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)  {
-
+        if(event.getTabKey() ==CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
