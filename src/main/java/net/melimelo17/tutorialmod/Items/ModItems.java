086ed26ef14ee00ac8babc2b9ce9aee75a7fda92
melimelo17 <maden.tiercepretet@hotmail.com>
2024-11-01 17:51:51 +0100

    Chisel Item

--- src/main/java/net/melimelo17/tutorialmod/Items/ModItems.java
+++ src/main/java/net/melimelo17/tutorialmod/Items/ModItems.java
@@ -1,5 +1,6 @@
 package net.melimelo17.tutorialmod.Items;
 
+import net.melimelo17.tutorialmod.Items.custom.ChiselItem;
 import net.melimelo17.tutorialmod.TutorialMod;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,7 +17,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
 
-
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
